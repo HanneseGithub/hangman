d6ec7152ed246dad3414a001e06f31f96ef459c4
Hannes Juurma <hannes@eagronom.com>
2020-11-23 17:38:43 +0200

    Add comments to the files.

--- game.js
+++ game.js
@@ -5,27 +5,31 @@ export default class Game {
   wordToGuess;
 
   constructor() {
+    // Assign the player 15 tries by default.
     this.lives = 15;
     this.wordToGuess = this.loadRandomWord();
   }
 
-  // Generate a random word from a random word API
+  // Call and save a random word from an API.
   async loadRandomWord() {
     let incomingData = await fetch("https://random-word.ryanrk.com/api/en/word/random")
     .then(function(response) {
       return response.json();
     })
     .then(json => {
+      // Transform the word to lowercase letters only.
       this.wordToGuess = json[0].toLowerCase();
     })
   }
 
-  // Create the gameboard
+  // Create the gameboard.
   loadGameBoard() {
+    // Clear the latest gameboard state.
     document.getElementById("gameboard").innerHTML = " ";
 
+    // For each character in the word, print something to the gameboard.
     for(let i = 0; i < this.wordToGuess.length; i++) {
-      // If there are characters inside correctCharacters, use them instead of "_".
+      // If the active character is inside correctCharacters[], use it instead of "_".
       if (this.correctCharacters.includes(this.wordToGuess[i])) {
         document.getElementById("gameboard").innerHTML += " " + this.wordToGuess[i] + " ";
       } else {
@@ -34,73 +38,80 @@ export default class Game {
     }
   }
 
-  // Deal with user's input
+  // Handle user's input after clicking the guessing button.
   handleUserInput(e) {
     event.preventDefault();
 
-    // Make sure the game doesn't go on after winning
+    // Make sure the game doesn't continue after the player has won.
     if(!document.getElementById("gameboard").innerHTML.includes("_")) {
       document.getElementById("win").innerHTML = "You won!";
       return;
     }
 
-    // Make sure the game doesn't go on after losing
+    // Make sure the game doesn't continue after the player has lost.
     if (this.lives <= 0) {
       document.getElementById("lost").innerHTML = "You lost!";
       return;
     }
 
-    // Reset error message after every click
+    // Reset error messages after every click.
     document.getElementById("error").innerHTML = "";
 
-    // Save user's guess
+    // Save user's input.
     let userInput = document.getElementById("guess").value.toLowerCase();
 
-    // Reset user input after every click
+    // Reset input field after every click.
     document.getElementById("guess").value = '';
 
-    // Make sure, that user inputs only one character
+    // Make sure, that user inputs only one character.
     if (userInput.length != 1) {
       document.getElementById("error").innerHTML = "You should only enter one character at a time!";
       return;
     }
 
-    // Make sure, that user inputs a string
+    // Make sure, that user inputs a string.
     if(!isNaN(userInput)) {
-    document.getElementById("error").innerHTML = "You should only enter string characters";
-    return;
+      document.getElementById("error").innerHTML = "You should only enter string characters";
+      return;
     }
 
-    // Check, if user input has already been used
+    // Check, if the character has already been used.
     if (this.usedCharacters.includes(userInput.toUpperCase())) {
       document.getElementById("error").innerHTML = "This character has been used already!";
       document.getElementById("guessedResult").innerHTML = this.usedCharacters;
       return;
     }
 
-    // Push new character to usedCharacters
+    // Push a new character to usedCharacters.
     this.usedCharacters.push(userInput.toUpperCase());
+    // Show guessed characters on the screen.
     document.getElementById("guessedResult").innerHTML = this.usedCharacters;
 
-    // Check, if the input was wrong
+    // Check, if user's guess was wrong.
     if(!this.wordToGuess.includes(userInput)) {
+      // If user guessed wrong, subtract one life and check if game is over or not.
       this.lives --;
+
+      // If the lives hit 0, let user know that he lost.
       if (this.lives <= 0) {
         document.getElementById("livesLeft").innerHTML = "Lives left: " + this.lives;
         document.getElementById("lost").innerHTML = "You lost!";
         return;
       }
+
+      // If there are enough lives left, let him know that the guess was wrong.
       document.getElementById("livesLeft").innerHTML = "Lives left: " + this.lives;
       document.getElementById("error").innerHTML = 'The character "' + userInput + '" is not in the word! You lost a life!'
       return;
     }
 
-    // Save correct character to the array and load the game board again, with updated characters.
+    // Save correctly guessed character to the array and
     this.correctCharacters.push(userInput);
+
+    // Load the game board again with updated characters.
     this.loadGameBoard(this.wordToGuess);
 
-
-    // If the string doesnt contain "_", win
+    // If the string doesnt contain "_", player has won.
     if (!document.getElementById("gameboard").innerHTML.includes("_")) {
       document.getElementById("win").innerHTML = "You won!";
     }
--- state.js
+++ state.js
@@ -1,4 +1,6 @@
 import Game from './game.js';
 
+// Create and save a new Game object.
 const theGame = new Game();
+// Export the initialized game object, allowing only one instance to be active at a time.
 export const game = () => theGame;
